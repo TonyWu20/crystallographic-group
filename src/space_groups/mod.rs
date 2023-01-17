7ccaaf642a7db2df3971d82031dd59f76130b75c
TonyWu20 <tony.w21@gmail.com>
2023-01-17 10:57:15 +0800

    Rename `PointGroup` to `SymmetryGroup` and remove the `symbol` field

--- src/space_groups/mod.rs
+++ src/space_groups/mod.rs
@@ -1,4 +1,12 @@
-use std::fmt::Display;
+use std::{fmt::Display, marker::PhantomData};
+
+use crate::{
+    crystal_symmetry_directions::Universal,
+    point_groups::{GroupBuilder, SymmetryGroup},
+    CrystalSystem, Standard, Triclinic,
+};
+
+use self::bravais_lattices::{BravaisLattice, P};
 
 mod bravais_lattices;
 
@@ -9,4 +17,42 @@ pub trait SpaceGroupProperties: Display {
     fn new() -> Self::Item;
     fn points_per_lattice(&self) -> u32;
     fn lattice_coordinates(&self) -> Vec<[f64; 3]>;
+}
+
+pub struct SpaceGroup<S: CrystalSystem, B: BravaisLattice> {
+    generators: SymmetryGroup,
+    symbol: String,
+    system: PhantomData<S>,
+    bravais: PhantomData<B>,
+}
+
+pub struct Undefined;
+impl CrystalSystem for Undefined {}
+impl BravaisLattice for Undefined {}
+
+pub struct SpaceGroupBuilder<S: CrystalSystem, B: BravaisLattice>(PhantomData<S>, PhantomData<B>);
+
+impl<S: CrystalSystem, B: BravaisLattice> SpaceGroupBuilder<S, B> {
+    pub fn new_builder() -> SpaceGroupBuilder<Undefined, Undefined> {
+        SpaceGroupBuilder(PhantomData, PhantomData)
+    }
+}
+
+impl SpaceGroupBuilder<Undefined, Undefined> {
+    pub fn triclinic() -> SpaceGroupBuilder<Triclinic, P> {
+        SpaceGroupBuilder(PhantomData, PhantomData)
+    }
+}
+
+impl SpaceGroupBuilder<Triclinic, P> {
+    pub fn p_1() -> SpaceGroup<Triclinic, P> {
+        let e = GroupBuilder::<Standard, 1>::new().e();
+        let p1 = e * e;
+        SpaceGroup {
+            generators: p1,
+            symbol: "P1".into(),
+            system: PhantomData,
+            bravais: PhantomData,
+        }
+    }
 }
